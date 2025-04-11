f5ff013948516df60e129d5b3c06dbfade461581
codeMachine12138 <dulin12138@163.com>
2025-04-11 11:51:28 +0800

    golang学习

---
+++ go/gth/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/test/gth/service"
+
+func main() {
+	// 查询区块
+	// service.QueryBlock()
+	// // 查询交易
+	// service.QueryTransaction()
+
+	// service.QueryReceipt()
+
+	// service.QueryBalance("0x0faa6a7E6E68C52efaE2fFBcC705D4343740bAE4")
+
+	// service.QueryContractBalance()
+
+	// service.SubscribeBlock()
+
+	service.DepolyContract()
+}
